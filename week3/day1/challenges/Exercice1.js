a04db0617c2264d212acb1ff46a98c80f0a83eab
chaimaa345 <chaimaelhajjami41@gmail.com>
2025-07-01 17:27:13 +0100

    update

---
+++ week3/day1/challenges/Exercice1.js
@@ -0,0 +1,20 @@
+// Example sentence
+let sentence = "The movie is not that bad, I like it";
+
+// Find index of "not"
+let wordNot = sentence.indexOf("not");
+
+// Find index of "bad"
+let wordBad = sentence.indexOf("bad");
+
+// Check if "bad" comes after "not"
+if (wordNot !== -1 && wordBad !== -1 && wordBad > wordNot) {
+  // Replace from "not" to "bad" (inclusive) with "good"
+  let newSentence = sentence.slice(0, wordNot) + "good" + sentence.slice(wordBad + 3);
+  console.log(newSentence);
+} else {
+  // Otherwise, print original sentence
+  console.log(sentence);
+}
+console.log(wordNot);
+console.log(wordBad);
